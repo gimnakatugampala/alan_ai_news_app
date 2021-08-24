b712c8862ab23ce8c1a20f74b142c37e52cf5b7b
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 19:37:41 -0700

    open article on voice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React,{useEffect, useState} from "react"
 import alanBtn from "@alan-ai/alan-sdk-web"
 
+import wordsToNumbers from "words-to-numbers"
+
 import NewsCards from "./components/NewsCards/NewsCards"
 import useStyles from './styles.js'
 
@@ -17,12 +19,25 @@ const App = () => {
 
         alanBtn({
             key:alanKey,
-            onCommand:({command,articles}) =>{
+            onCommand:({command,articles,number}) =>{
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles)
                     setActiveArticle(-1)
                 }else if(command === 'highlight'){
                     setActiveArticle((prevActiveArticle) => prevActiveArticle +1)
+                }else if(command === 'open'){
+                    const parsedNumber = number.length > 2 ?  wordsToNumbers(number,{fuzzy:true}) : number;
+
+                    const article = articles[parsedNumber -1]
+
+                    if(parsedNumber > 20){
+                        alanBtn().platText('Please try that again.')
+                    }else if (article){
+                        
+                        window.open(article.url ,'_blank')
+                        alanBtn().playText('Opening..')
+                    }
+
                 }
             }
 
