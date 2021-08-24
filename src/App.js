186bed6038c9cb57ffd3de076533e5eb1dc37f92
Gimna Katugampala <autoservicelk@gmail.com>
2021-08-23 18:26:19 -0700

    added the styles of App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React,{useEffect, useState} from "react"
 import alanBtn from "@alan-ai/alan-sdk-web"
 
 import NewsCards from "./components/NewsCards/NewsCards"
+import useStyles from './styles.js'
 
 const alanKey = 'bd6b7f79557736ec573ceda1e25300f72e956eca572e1d8b807a3e2338fdd0dc/stage'
 
 const App = () => {
+
+    const classes = useStyles()
 
     const [newsArticles,setNewsArticles] = useState([]);
 
@@ -25,7 +28,9 @@ const App = () => {
 
     return (
         <div>
-            <h1>Alan AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+            </div>
             <NewsCards articles={newsArticles} />
         </div>
     )
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -6,5 +6,19 @@ export default makeStyles({
         width:'100%',
         margin:0,
 
-    }
+    },
+    card: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+        height: '45vh',
+        padding: '10%',
+        borderRadius: 10,
+        color: 'white',
+      },
+      infoCard: {
+        display: 'flex', flexDirection: 'column', textAlign: 'center',
+      },
 })
